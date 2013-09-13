0f16a7c8c3018138b253933f387964dc1e90c200
Priya <mkimzie@gmail.com>
2013-09-12 21:34:10 -0400

    Added locale to the descriptors.

--- calculateDescriptors.m
+++ calculateDescriptors.m
@@ -1,3 +1,10 @@
 function descriptors = calculateDescriptors(I, keypoints)
+    % Calculate the normal SURF descriptors
     descriptors = extractFeatures(I, keypoints, 'Method', 'SURF', 'SURFSize', 128);
+    
+    % Add the normalized location to the descriptors.
+    location(:, 1) = keypoints(:, 1)/size(I, 2);
+    location(:, 2) = keypoints(:, 2)/size(I, 1);
+    
+    descriptors = [descriptors location];
 end
