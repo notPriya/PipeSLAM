5025ef00770286b7bf4a83f5b760b1a2152e44d2
Priya <mkimzie@gmail.com>
2013-12-06 20:06:39 -0500

    Added options to matchSIFT.
    You can choose between svm, mahalanobis, and covariance matchSIFT
    algorithms.

--- matchSIFT.m
+++ matchSIFT.m
@@ -1,5 +1,41 @@
-function [matches] = matchSIFT(D1, D2, t)
-    load('diag_covariance.mat');
+function [matches] = matchSIFT(D1, D2, t, type, A_ind)
+if type == 1
+    load('svm_linear_model.mat');
+    
+    n = size(D1, 2);
+    max_val = zeros(n, 1);
+    max_ind = zeros(n, 1);
+    
+    d2 = D2';
+    for i = 1:n
+        d = repmat(D1(:,i)', size(d2, 1), 1);
+        test = double((d - d2).^2);
+        testY = ones(size(test, 1), 1);
+        
+        [~, ~, p] = svmpredict(testY, test, mdl, '-b 1');
+        
+        [val, ind] = max(p(:,1));        
+        max_val(i) = val;
+        max_ind(i) = ind;
+    end
+
+    index = find(max_val > t);
+
+    % Make sure we get at least 20 matches. Take the top 20 matches if
+    % there are not enough.
+    if (length(index) < 20)
+        disp('Not enough matches');
+    end
+    
+    matches = [index'; max_ind(index)'];
+    
+else
+    if type == 2
+        load('itml_results.mat');
+        S = A{A_ind};
+    else
+        load('diag_covariance.mat');
+    end
     
     % Compute distances from everything in D1 to everything in D2
     distances = pdist2(D1', D2', 'mahalanobis', S);
@@ -14,7 +50,7 @@ function [matches] = matchSIFT(D1, D2, t)
     distances(index) = NaN;
     
     % Find second nearnest neighbor.
-    [min2_val, min2_ind] = min(distances, [], 2);
+    min2_val = min(distances, [], 2);
     
     % Compute thresholded value.
     threshold = min_val ./ min2_val;
@@ -31,4 +67,5 @@ function [matches] = matchSIFT(D1, D2, t)
     
     % Compute result
     matches = [index'; min_ind(index)'];
+end
 end
