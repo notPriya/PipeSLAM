42fd4cde7c64e7e3aa816fed601b2b9d7ceea367
notPriya <xb1ad3x>
2014-01-22 13:43:42 -0500

    Cleaned up transformVideo.m

--- transformVideo.m
+++ transformVideo.m
@@ -6,10 +6,6 @@ clc;
 % obj = VideoReader('pipe1.mp4');
 % frames = read(obj);
 load('pipe1.mat');
-
-% start_frame = 1;
-% end_frame = 670;
-% num_frames = end_frame - start_frame;
 n = size(frames, 4);
 
 % Get the odometry bounding box.
@@ -23,26 +19,6 @@ disp('Starting preprocessing');
 % Preprocess the data for bundle adjustment.
 %%%%
 [K, D, M, Hto1] = preprocessBAData(frames, odom_rect, n);
-
-%%%%
-% Draw the robot position and orientation.
-%%%%
-figure;
-hold on;
-for i=1:n
-    o = Hto1{i}(1:3, 4);
-    x = Hto1{i}(1:3, 1)*.1 + o;
-    y = Hto1{i}(1:3, 2)*.1 + o;
-    z = Hto1{i}(1:3, 3)*.1 + o;
-    plot3([o(1) x(1)], [o(2) x(2)], [o(3) x(3)], 'r');
-    plot3([o(1) y(1)], [o(2) y(2)], [o(3) y(3)], 'g');
-    plot3([o(1) z(1)], [o(2) z(2)], [o(3) z(3)], 'b');
-    plot3(o(1), o(2), o(3), 'o', 'MarkerSize', 2, 'LineWidth', 2);    
-end
-plot3(o(1), o(2), o(3), 'rx', 'MarkerSize', 15, 'LineWidth', 2);
-plot3(0, 0, 0, 'gx', 'MarkerSize', 15, 'LineWidth', 2);
-axis equal;
-
 old_Hto1 = Hto1;
 
 disp('Preprocessing complete. Starting bundle adjustment.');
@@ -66,7 +42,7 @@ for i=1:n-3
     end
 end
 
-%% Draw the robot position and orientation.
+%% Draw the adjusted robot position and orientation.
 figure;
 hold on;
 
@@ -85,10 +61,28 @@ plot3(o(1), o(2), o(3), 'rx', 'MarkerSize', 15, 'LineWidth', 2);
 plot3(0, 0, 0, 'gx', 'MarkerSize', 15, 'LineWidth', 2);
 axis equal;
 
+%% Draw the original robot position and orientation.
+% figure;
+% hold on;
+
+for i=1:n
+    o = old_Hto1{i}(1:3, 4);
+    x = old_Hto1{i}(1:3, 1)*.1 + o;
+    y = old_Hto1{i}(1:3, 2)*.1 + o;
+    z = old_Hto1{i}(1:3, 3)*.1 + o;
+    plot3([o(1) x(1)], [o(2) x(2)], [o(3) x(3)], 'r');
+    plot3([o(1) y(1)], [o(2) y(2)], [o(3) y(3)], 'g');
+    plot3([o(1) z(1)], [o(2) z(2)], [o(3) z(3)], 'b');
+    plot3(o(1), o(2), o(3), 'ro', 'MarkerSize', 2, 'LineWidth', 2);    
+end
+
+plot3(o(1), o(2), o(3), 'rx', 'MarkerSize', 15, 'LineWidth', 2);
+plot3(0, 0, 0, 'gx', 'MarkerSize', 15, 'LineWidth', 2);
+axis equal;
+
 %% Save off the z-coordinate positions
 
-zs = zeros(num_frames+1, 1);
-for i=2:num_frames+1
-    o = Hto1{i-1}(1:3, 4);
-    zs(i) = o(3);
+zs = zeros(n, 1);
+for i=n
+    zs(i) = Hto1{i}(3, 4);
 end
